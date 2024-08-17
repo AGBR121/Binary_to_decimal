6e6377d3efde774a10b54dabc7edc44d7abce113
AGBR121 <angel.burbano@utp.edu.co>
2024-08-17 17:27:18 -0500

    campos de formulario sin css ni funcion

--- src/components/binariodecimal.jsx
+++ src/components/binariodecimal.jsx
@@ -4,6 +4,20 @@ export default function BinarioDecimal(){
     return(
         <div>
             <Navbar/>
+            <h4>Pasar de sistema decimal a binario</h4>
+            <div className='Form'>
+                <form>
+                    <label>
+                        Decimal
+                        <input type='number' required/>
+                    </label>
+                    <button type='submit'>Convertir</button>
+                </form>
+            </div>
+            <div>
+        <p>Resultado:</p>
+            <input type="text" readOnly />
+        </div>
         </div>
     )
 }

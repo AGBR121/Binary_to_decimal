521f6e94e7e3af7cb043a4295084a46c611dd672
AGBR121 <angel.burbano@utp.edu.co>
2024-08-17 19:22:04 -0500

    form con estilo y sin funcionalidad

--- src/components/binariodecimal.jsx
+++ src/components/binariodecimal.jsx
@@ -1,22 +1,30 @@
 import React, { useState } from 'react';
-import Navbar from './navbar';
+import Navbar from './navbar.jsx';
+import '../styles/form.css';
 export default function BinarioDecimal(){
     return(
         <div>
             <Navbar/>
+            <div className='form'>
             <h4>Pasar de sistema binario a decimal</h4>
+            <br></br>
             <div className='Form'>
                 <form>
                     <label>
-                        Decimal
+                        Binario
                         <input type='number' required/>
                     </label>
+                    <div className='button-container'>
                     <button type='submit'>Convertir</button>
+                    </div>
                 </form>
             </div>
             <div>
-        <p>Resultado:</p>
+        <label>
+            Respuesta
             <input type="text" readOnly />
+        </label>
+        </div>
         </div>
         </div>
     )
--- src/components/decimalbinario.jsx
+++ src/components/decimalbinario.jsx
@@ -1,22 +1,30 @@
 import React, { useState } from 'react';
 import Navbar from './navbar.jsx';
+import '../styles/form.css';
 export default function DecimalBinario(){
     return(
         <div>
             <Navbar/>
+            <div className='form'>
             <h4>Pasar de sistema decimal a binario</h4>
+            <br></br>
             <div className='Form'>
                 <form>
                     <label>
                         Decimal
                         <input type='number' required/>
                     </label>
+                    <div className='button-container'>
                     <button type='submit'>Convertir</button>
+                    </div>
                 </form>
             </div>
             <div>
-        <p>Resultado:</p>
+        <label>
+            Respuesta
             <input type="text" readOnly />
+        </label>
+        </div>
         </div>
         </div>
     )

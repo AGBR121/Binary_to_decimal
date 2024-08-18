e2f882472b62221bd5a627b9f483b4544fe94a3e
AGBR121 <angel.burbano@utp.edu.co>
2024-08-17 21:07:15 -0500

    added page not found

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Home from './components/home.jsx';
 import DecimalBinario from './components/decimalbinario.jsx';
 import BinarioDecimal from './components/binariodecimal.jsx';
+import NotFound from './components/notfound.jsx';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/binario-decimal" element={<BinarioDecimal/>} />
         <Route path="/decimal-binario" element={<DecimalBinario/>} />
-        <Route path="*" element={<notFound/>} />
+        <Route path="*" element={<NotFound/>} />
       </Routes>
     </Router>
   )

4224e1d39f9c6e69ebb45da17f3c8c7b19618c96
AGBR121 <angel.burbano@utp.edu.co>
2024-08-17 21:00:05 -0500

    Complete

--- src/components/binariodecimal.jsx
+++ src/components/binariodecimal.jsx
@@ -1,31 +1,54 @@
 import React, { useState } from 'react';
 import Navbar from './navbar.jsx';
 import '../styles/form.css';
-export default function BinarioDecimal(){
-    return(
+
+export default function BinarioDecimal() {
+    const [binary, setBinary] = useState('');
+    const [decimal, setDecimal] = useState('');
+
+    const handleBinaryChange = (e) => {
+        setBinary(e.target.value);
+    };
+
+    const handleConvert = (e) => {
+        e.preventDefault();
+        if (/^[01]+$/.test(binary)) { 
+            setDecimal(parseInt(binary, 2));
+        } else {
+            setDecimal('NAN');
+        }
+    };
+
+    return (
         <div>
-            <Navbar/>
+            <Navbar />
             <div className='form'>
-            <h4>Pasar de sistema binario a decimal</h4>
-            <br></br>
-            <div className='Form'>
-                <form>
+                <h4>Pasar de sistema binario a decimal</h4>
+                <br />
+                <div className='Form'>
+                    <form onSubmit={handleConvert}>
+                        <label>
+                            Binario
+                            <input 
+                                type='text' 
+                                value={binary} 
+                                onChange={handleBinaryChange} 
+                                placeholder="Ingrese un número binario" 
+                                required
+                            />
+                        </label>
+                        <div className='button-container'>
+                            <button type='submit'>Convertir</button>
+                        </div>
+                    </form>
+                </div>
+                <div>
                     <label>
-                        Binario
-                        <input type='number' required/>
+                        Respuesta
+                        <input type="text" value={decimal} readOnly />
                     </label>
-                    <div className='button-container'>
-                    <button type='submit'>Convertir</button>
-                    </div>
-                </form>
+                </div>
             </div>
-            <div>
-        <label>
-            Respuesta
-            <input type="text" readOnly />
-        </label>
         </div>
-        </div>
-        </div>
-    )
+    );
 }
--- src/components/decimalbinario.jsx
+++ src/components/decimalbinario.jsx
@@ -1,31 +1,55 @@
 import React, { useState } from 'react';
 import Navbar from './navbar.jsx';
 import '../styles/form.css';
+
 export default function DecimalBinario(){
+    const [decimal, setDecimal] = useState('');
+    const [binary, setBinary] = useState('');
+
+    const handleDecimalChange = (e) => {
+        setDecimal(e.target.value);
+    };
+
+    const handleConvert = () => {
+        if (!isNaN(decimal) && decimal !== '') {
+            setBinary(decimalToBinary(Number(decimal)));
+        } else {
+            setBinary('Entrada inválida');
+        }
+    };
+
+    const decimalToBinary = (decimal) => {
+        return (decimal >>> 0).toString(2);
+    };
+
     return(
         <div>
             <Navbar/>
             <div className='form'>
-            <h4>Pasar de sistema decimal a binario</h4>
-            <br></br>
-            <div className='Form'>
-                <form>
+                <h4>Pasar de sistema decimal a binario</h4>
+                <br/>
+                <div className='Form'>
                     <label>
                         Decimal
-                        <input type='number' required/>
+                        <input 
+                            type='number' 
+                            value={decimal}  
+                            onChange={handleDecimalChange} 
+                            placeholder="Ingrese un número decimal" 
+                            required
+                        />
                     </label>
                     <div className='button-container'>
-                    <button type='submit'>Convertir</button>
+                        <button type='button' onClick={handleConvert} >Convertir</button>
                     </div>
-                </form>
+                </div>
+                <div>
+                    <label>
+                        Respuesta
+                        <input type="text" value={binary} readOnly />
+                    </label>
+                </div>
             </div>
-            <div>
-        <label>
-            Respuesta
-            <input type="text" readOnly />
-        </label>
         </div>
-        </div>
-        </div>
-    )
+    );
 }
